277728075938532edad8c43a53655ae571a695f6
Icaro de Barros <icarodebarros@gmail.com>
2023-01-21 10:27:16 -0300

    Adding Author type

--- schema/schema.js
+++ schema/schema.js
@@ -1,14 +1,29 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { 
+  GraphQLObjectType, 
+  GraphQLID, // make the query more flexible so you can pass a number or string as ID
+  GraphQLString, 
+  GraphQLSchema,
+  GraphQLInt
+} = graphql;
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
-  })
+    genre: { type: GraphQLString },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  }),
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -17,16 +32,34 @@ const RootQuery = new GraphQLObjectType({
     book: {
       type: BookType,
       args: {
-        id: { type: GraphQLString }
+        id: { type: GraphQLID },
       },
       resolve(parent, args) {
         // code to get data from db/other source
-        return { id: "id"+args.id, name: "name"+args.id, genre: "genre"+args.id }
-      }
+        return {
+          id: 'id' + args.id,
+          name: 'name' + args.id,
+          genre: 'genre' + args.id,
+        };
+      },
     },
-  }
+    author: {
+      type: AuthorType,
+      args: {
+        id: { type: GraphQLID }
+      },
+      resolve(parent, args) {
+        // code to get data from db/other source
+        return {
+          id: 'id' + args.id,
+          name: 'name' + args.id,
+          age: 40 + +args.id,
+        };
+      }
+    }
+  },
 });
 
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
 });
