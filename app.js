e5b798398b553d44b62c1e994c9049402736b58f
Icaro de Barros <icarodebarros@gmail.com>
2023-01-21 11:32:52 -0300

    Connecting to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -9,6 +10,15 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }));
 
-app.listen(4000, () => {
-  console.log('Now listening for requests on port 4000');
-})
+
+mongoose
+  .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.gafjw.mongodb.net/?retryWrites=true&w=majority`)
+  .then(_result => {
+    app.listen(4000, () => {
+      console.log('Now listening for requests on port 4000');
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
+mongoose.connection.once('open', () => console.log('Connected to database!'));
