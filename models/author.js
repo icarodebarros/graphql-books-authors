441cfa209064da9d6182f0c5bc10b011cf127535
Icaro de Barros <icarodebarros@gmail.com>
2023-01-25 10:18:15 -0300

    Creating book and author schemas

---
+++ models/author.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const authorSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = mongoose.models('Author', authorSchema);
