e1fca010d1a6f8625c50cec83847f676543e45d6
Icaro de Barros <icarodebarros@gmail.com>
2023-01-30 12:43:43 -0300

    Adjusting response headers to avoid CORS errors

--- server/app.js
+++ server/app.js
@@ -5,6 +5,19 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   graphiql: true
