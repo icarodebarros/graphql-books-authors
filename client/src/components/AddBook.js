bdfae7ce2c9ff6cc73944f75210c2536aa7dd379
Icaro de Barros <icarodebarros@gmail.com>
2023-01-30 14:21:33 -0300

    Controlling form inputs

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,16 +1,21 @@
 import { useQuery } from '@apollo/client';
+import { useState } from 'react';
 import { GET_AUTHORS } from '../queries/queries';
 
 function AddBook() {
   const { loading, error, data } = useQuery(GET_AUTHORS);
+  const [book, setBook] = useState({ name: '', genre: '', authorId: '' });
+
+  const onSubmitHandler = (ev) => {
+    ev.preventDefault();
+    console.log('book', book);
+  };
 
   let options;
   if (loading) {
     options = <option disabled>Loading authors</option>;
-  
   } else if (error) {
     options = <option disabled>Error : {error.message}</option>;
-  
   } else if (data && data.authors) {
     options = data.authors.map((author) => {
       return (
@@ -22,18 +27,34 @@ function AddBook() {
   }
 
   return (
-    <form id="add-book">
+    <form id="add-book" onSubmit={onSubmitHandler}>
       <div className="field">
         <label>Book name:</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={book.name}
+          onChange={(ev) =>
+            setBook((curBook) => ({ ...curBook, name: ev.target.value }))
+          }
+        />
       </div>
       <div className="field">
         <label>Genre:</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={book.genre}
+          onChange={(ev) =>
+            setBook((curBook) => ({ ...curBook, genre: ev.target.value }))
+          }
+        />
       </div>
       <div className="field">
         <label>Author:</label>
-        <select>
+        <select
+          onChange={(ev) =>
+            setBook((curBook) => ({ ...curBook, authorId: ev.target.value }))
+          }
+        >
           <option>Select author</option>
           {options}
         </select>
