c17d098ae2d5d91d6ce50092f8210674e2ff5973
Icaro de Barros <icarodebarros@gmail.com>
2023-01-30 12:12:28 -0300

    Creating base components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+import BookList from './components/BookList';
+
 function App() {
   return (
     <div id="main">
-      <h1>Ninja's Reading List</h1>
+      <h1>Reading List</h1>
+      <BookList />
     </div>
   );
 }
---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+
+function BookList() {
+  return (
+    <Fragment>
+      <ul id="book-list">
+        <li>Book name</li>
+      </ul>
+    </Fragment>
+  );
+}
+
+export default BookList;
